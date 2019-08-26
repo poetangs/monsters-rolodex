f296e2522c746be69726823fb282c0499b6ccfe1
poetangs <poetangs2005@gmail.com>
2019-08-26 21:33:28 +0700

    Updates

---
+++ precache-manifest.ee7e42efae30bfa1f2de8ebfc2ace7af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71db6432b2a9fbb82f45990fe2e441ca",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bd12446b442f1d7af2cf",
+    "url": "/monsters-rolodex/static/css/main.5c29549d.chunk.css"
+  },
+  {
+    "revision": "4b7594f9ba93f757104a",
+    "url": "/monsters-rolodex/static/js/2.765d0f25.chunk.js"
+  },
+  {
+    "revision": "bd12446b442f1d7af2cf",
+    "url": "/monsters-rolodex/static/js/main.214a95d3.chunk.js"
+  },
+  {
+    "revision": "f7fff3448ef51b7cb626",
+    "url": "/monsters-rolodex/static/js/runtime~main.edd7f36b.js"
+  }
+]);
